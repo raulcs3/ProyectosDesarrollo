241fd46b03af7eb0455cb15f3f6b4413875754c5
raulcs3 <58291745+raulcs3@users.noreply.github.com>
2023-08-05 11:47:18 +0200

    v1.2

--- Proyecto1/GeneradorContraseñas.py
+++ Proyecto1/GeneradorContraseñas.py
@@ -4,8 +4,6 @@ import random
 def generar_contrasena():
     longitud = int(input("Ingrese la longitud de la contraseña: "))
     
-    # Caracteres a utilizar
-
     caracteres = ""
     if input("¿Incluir caracteres en minúsculas? (s/n): ").lower() == "s":
         caracteres += string.ascii_lowercase
@@ -16,14 +14,9 @@ def generar_contrasena():
     if input("¿Incluir caracteres especiales? (s/n): ").lower() == "s":
         caracteres += string.punctuation
     
-    # Verificar que el usuario haya seleccionado al menos un tipo de caracter
-
-    if not caracteres:
         print("Debe seleccionar al menos un tipo de caracter.")
         return
     
-    # Generar la contraseña aleatoria
-
     contraseña = ''.join(random.choice(caracteres) for i in range(longitud))
     print("Contraseña generada:", contraseña)
 
